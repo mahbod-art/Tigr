8b2ec00805a45d2f2cc03bcc8d26c25f1a38a64f
mahbod-art <mafar001@ucr.edu>
2021-01-08 17:18:14 -0800

    u

--- tigr/sssp.cu
+++ tigr/sssp.cu
@@ -176,12 +176,12 @@ int main(int argc, char** argv)
 
 	} while (!(finished));
 	
-	cout << "Number of iterations = " << itr << endl;
+	//cout << "Number of iterations = " << itr << endl;
 	cout << itr << endl;
 
 	
 	float runtime = t.Finish();
-	cout << "Processing finished in " << runtime << " (ms).\n";
+	//cout << "Processing finished in " << runtime << " (ms).\n";
 	cout << runtime << "\n";
 
 	gpuErrorcheck(cudaMemcpy(dist, d_dist, num_nodes*sizeof(unsigned int), cudaMemcpyDeviceToHost));
