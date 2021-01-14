5c1a3e7ce4afe2882bfb5be864fda731cb48d898
mahbod-art <mafar001@ucr.edu>
2021-01-14 05:17:52 -0800

    u

--- tigr/sssp.cu
+++ tigr/sssp.cu
@@ -63,7 +63,7 @@ __global__ void kernel(unsigned int numParts,
 		}
 	
 	}
-	printf("Edge Processed: \n", Edge_Processed); 
+	printf("Edge Processed: %d\n", Edge_Processed); 
 }
 
 __global__ void clearLabel(bool *label, unsigned int size)
